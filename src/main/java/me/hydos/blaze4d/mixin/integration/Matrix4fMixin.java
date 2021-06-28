2e1eb4c815a68a2a5eab3ce5e2395f063d09f3f3
hYdos <haydenv06@gmail.com>
2021-06-28 15:34:07 +1000

    document projection matrix

--- src/main/java/me/hydos/blaze4d/mixin/integration/Matrix4fMixin.java
+++ src/main/java/me/hydos/blaze4d/mixin/integration/Matrix4fMixin.java
@@ -14,15 +14,15 @@ public class Matrix4fMixin {
     @Inject(method = "projectionMatrix(FFFFFF)Lnet/minecraft/util/math/Matrix4f;", at = @At("HEAD"), cancellable = true)
     private static void createVkProjectionMatrix(float left, float right, float bottom, float top, float nearPlane, float farPlane, CallbackInfoReturnable<Matrix4f> cir) {
         Matrix4f proj = new Matrix4f();
-        float f = right - left;
-        float g = bottom + top;
-        float h = farPlane - nearPlane;
-        proj.a00 = 2.0F / f;
-        proj.a11 = 2.0F / g;
-        proj.a22 = -2.0F / h;
-        proj.a03 = -(right + left) / f;
-        proj.a13 = -(bottom + top) / g;
-        proj.a23 = -(farPlane + nearPlane) / h;
+        float x = right - left;
+        float y = bottom + top;
+        float focalLength = farPlane - nearPlane;
+        proj.a00 = 2.0F / x;
+        proj.a11 = 2.0F / y;
+        proj.a22 = -2.0F / focalLength;
+        proj.a03 = -(right + left) / x;
+        proj.a13 = -(bottom + top) / y;
+        proj.a23 = -(farPlane + nearPlane) / focalLength;
         proj.a33 = 1.0F;
         GlobalRenderSystem.projectionMatrix = MinecraftUbo.toJoml(proj);
         cir.setReturnValue(proj);
