3378aae9412b8f68fbaa6367de7915ed4b3296f6
OroArmor <eliorona@live.com>
2021-07-01 22:46:56 -0700

    Uniforms are now added dynamically, need to work on not uploading unneeded memory

---
+++ src/main/java/me/hydos/blaze4d/mixin/shader/ShaderMixin.java
@@ -0,0 +1,34 @@
+package me.hydos.blaze4d.mixin.shader;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import me.hydos.blaze4d.api.shader.OpenGLToVulkanShaderProcessor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.render.Shader;
+import net.minecraft.client.render.VertexFormat;
+import net.minecraft.resource.ResourceFactory;
+
+@Mixin(Shader.class)
+public class ShaderMixin {
+    private static ShaderAccessor blaze4d$capturedShader = null;
+
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/Shader;readBlendState(Lcom/google/gson/JsonObject;)Lnet/minecraft/client/gl/GlBlendState;"))
+    public void captureShaderForStaticMethods(ResourceFactory factory, String name, VertexFormat format, CallbackInfo ci) {
+        blaze4d$capturedShader = (ShaderAccessor) this;
+    }
+
+    @ModifyArg(method = "loadProgram", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/Program;createFromResource(Lnet/minecraft/client/gl/Program$Type;Ljava/lang/String;Ljava/io/InputStream;Ljava/lang/String;Lnet/minecraft/client/gl/GLImportProcessor;)Lnet/minecraft/client/gl/Program;"), index = 2)
+    private static InputStream no(InputStream stream) throws IOException {
+        String originalSource = new String(stream.readAllBytes());
+        String transformedToVulkan = String.join("\n", OpenGLToVulkanShaderProcessor.convertOpenGLToVulkanShader(List.of(originalSource), blaze4d$capturedShader.blaze4d$getUniforms()));
+        return new ByteArrayInputStream(transformedToVulkan.getBytes());
+    }
+}
