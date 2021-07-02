81916aefe781b0f32daf31bc1dac5386ae18a7cf
hYdos <haydenv06@gmail.com>
2021-07-02 12:45:22 +1000

    make world loading stable

---
+++ src/main/java/me/hydos/blaze4d/mixin/debug/TextureManagerMixin.java
@@ -0,0 +1,16 @@
+package me.hydos.blaze4d.mixin.debug;
+
+import net.minecraft.client.texture.TextureManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TextureManager.class)
+public class TextureManagerMixin {
+
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    private void e(CallbackInfo ci) {
+        ci.cancel();
+    }
+}
