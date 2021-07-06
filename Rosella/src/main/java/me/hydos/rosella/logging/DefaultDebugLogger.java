cde131050821ae6a484145c85ee2fb8ea050dae0
hYdos <haydenv06@gmail.com>
2021-07-06 11:27:07 +1000

    fix object loading

--- Rosella/src/main/java/me/hydos/rosella/logging/DefaultDebugLogger.java
+++ Rosella/src/main/java/me/hydos/rosella/logging/DefaultDebugLogger.java
@@ -1,26 +1,32 @@
 package me.hydos.rosella.logging;
 
+import me.hydos.rosella.Rosella;
+
 import static org.lwjgl.vulkan.VK10.VK_FALSE;
 
 public class DefaultDebugLogger implements DebugLogger {
 
     @Override
     public int logValidation(String message, String severity) {
+        Rosella.LOGGER.error(message);
         return VK_FALSE;
     }
 
     @Override
     public int logPerformance(String message, String severity) {
+        Rosella.LOGGER.warn(message);
         return VK_FALSE;
     }
 
     @Override
     public int logGeneral(String message, String severity) {
+        Rosella.LOGGER.info(message);
         return VK_FALSE;
     }
 
     @Override
     public int logUnknown(String message, String severity) {
+        Rosella.LOGGER.info(message);
         return VK_FALSE;
     }
 }
