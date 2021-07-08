8f0217178edffe9caa0057828cd39380a1ded6ae
Ramid Khan <ramidzkh@gmail.com>
2021-07-08 15:16:11 +1000

    Start migrating to better language

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+
+        maven {
+            name = "FabricMC"
+            url = uri("https://maven.fabricmc.net/")
+        }
+
+        maven {
+            name = "Cotton"
+            url = uri("https://server.bbkr.space/artifactory/libs-release/")
+        }
+    }
+}
+
+include("Rosella")
+include("Aftermath")
