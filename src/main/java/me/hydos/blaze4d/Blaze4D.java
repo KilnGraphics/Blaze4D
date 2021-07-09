56de65c0a7b45ca69bc37c3b3f9586d9f34c1cf0
burgerguy <30326913+burgerguy@users.noreply.github.com>
2021-07-09 01:14:18 -0400

    make blank texture correct type

--- src/main/java/me/hydos/blaze4d/Blaze4D.java
+++ src/main/java/me/hydos/blaze4d/Blaze4D.java
@@ -14,7 +14,7 @@ public class Blaze4D implements ClientModInitializer {
 
     public static final Logger LOGGER = LogManager.getLogger("Blaze4D", new StringFormatterMessageFactory());
     public static final boolean VALIDATION_ENABLED = false;
-    public static final boolean RENDERDOC_ENABLED = true;
+    public static final boolean RENDERDOC_ENABLED = false;
 
     public static Rosella rosella;
     public static GlfwWindow window;
