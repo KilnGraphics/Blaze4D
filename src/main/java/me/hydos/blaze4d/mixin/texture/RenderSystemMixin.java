23b18af7df8211adc16c80ade7d9605bf564db68
burgerguy <30326913+burgerguy@users.noreply.github.com>
2021-07-11 21:31:40 -0400

    fix textures slightly

--- src/main/java/me/hydos/blaze4d/mixin/texture/RenderSystemMixin.java
+++ src/main/java/me/hydos/blaze4d/mixin/texture/RenderSystemMixin.java
@@ -10,15 +10,32 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RenderSystem.class)
 public class RenderSystemMixin {
 
-    @Inject(method = "setShaderTexture(ILnet/minecraft/util/Identifier;)V", remap = false, at = @At("HEAD"), cancellable = true)
-    private static void setTexture(int i, Identifier identifier, CallbackInfo ci) { // TODO: maybe don't ignore layer id?
-        TextureManager textureManager = MinecraftClient.getInstance().getTextureManager();
-        AbstractTexture abstractTexture = textureManager.getTexture(identifier);
-        GlobalRenderSystem.boundTextureIds[GlobalRenderSystem.activeTexture] = abstractTexture.getGlId();
+    @Inject(method = "setShaderTexture(ILnet/minecraft/util/Identifier;)V", at = @At("HEAD"), cancellable = true)
+    private static void setTextureFromIdentifier(int i, Identifier identifier, CallbackInfo ci) {
+        if (i >= 0 && i < GlobalRenderSystem.boundTextureIds.length) {
+            TextureManager textureManager = MinecraftClient.getInstance().getTextureManager();
+            AbstractTexture abstractTexture = textureManager.getTexture(identifier);
+            GlobalRenderSystem.boundTextureIds[i] = abstractTexture.getGlId();
+        }
         ci.cancel();
+    }
+
+
+    @Inject(method = "setShaderTexture(II)V", at = @At("HEAD"), cancellable = true)
+    private static void setTextureFromId(int i, int j, CallbackInfo ci) {
+        if (i >= 0 && i < GlobalRenderSystem.boundTextureIds.length) {
+            GlobalRenderSystem.boundTextureIds[i] = j;
+        }
+        ci.cancel();
+    }
+
+    @Inject(method = "getShaderTexture", at = @At("HEAD"), cancellable = true)
+    private static void getTextureFromUs(int i, CallbackInfoReturnable<Integer> cir) {
+        cir.setReturnValue(i >= 0 && i < GlobalRenderSystem.boundTextureIds.length ? GlobalRenderSystem.boundTextureIds[i] : 0);
     }
 }
