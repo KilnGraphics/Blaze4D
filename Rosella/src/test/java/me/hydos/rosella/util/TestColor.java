0ef925c993d15b91eb743d802ed42bfd0b30ea7c
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2021-07-15 20:11:57 +0200

    Added example test using color class

---
+++ Rosella/src/test/java/me/hydos/rosella/util/TestColor.java
@@ -0,0 +1,25 @@
+package me.hydos.rosella.util;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestColor {
+
+    @Test
+    void testAsFloats() {
+        Color c = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        assertEquals(1.0f, c.rAsFloat(), 1e-6f);
+        assertEquals(1.0f, c.gAsFloat(), 1e-6f);
+        assertEquals(1.0f, c.bAsFloat(), 1e-6f);
+
+        c = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+        assertEquals(0.0f, c.rAsFloat(), 1e-6f);
+        assertEquals(0.0f, c.gAsFloat(), 1e-6f);
+        assertEquals(0.0f, c.bAsFloat(), 1e-6f);
+
+        c = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+        assertEquals(0.5f, c.rAsFloat(), 1e-2f);
+        assertEquals(0.5f, c.gAsFloat(), 1e-2f);
+        assertEquals(0.5f, c.bAsFloat(), 1e-2f);
+    }
+}
