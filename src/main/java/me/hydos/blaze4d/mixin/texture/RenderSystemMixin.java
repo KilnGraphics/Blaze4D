14addb9d8eb607097e074a73d1fca2df75b89fa4
hYdos <haydenv06@gmail.com>
2021-07-15 18:57:15 +1000

    fix runtime with ugly mixin hack

--- src/main/java/me/hydos/blaze4d/mixin/texture/RenderSystemMixin.java
+++ src/main/java/me/hydos/blaze4d/mixin/texture/RenderSystemMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(value = RenderSystem.class, remap = false)
 public class RenderSystemMixin {
 
-    @Inject(method = "setShaderTexture(ILnet/minecraft/util/Identifier;)V", remap = false, at = @At("HEAD"), cancellable = true)
+    @Inject(method = "setShaderTexture(ILnet/minecraft/util/Identifier;)V", at = @At("HEAD"), require = 0, cancellable = true)
     private static void setTextureFromIdentifier(int i, Identifier identifier, CallbackInfo ci) {
         if (i >= 0 && i < GlobalRenderSystem.boundTextureIds.length) {
             TextureManager textureManager = MinecraftClient.getInstance().getTextureManager();
@@ -25,8 +25,17 @@ public class RenderSystemMixin {
         ci.cancel();
     }
 
+    @Inject(method = "setShaderTexture(ILnet/minecraft/class_2960;)V", at = @At("HEAD"), require = 0, cancellable = true) // ugly hack to get around mixin not remapping properly
+    private static void setTextureFromIdentifierInIntermediary(int i, Identifier identifier, CallbackInfo ci) {
+        if (i >= 0 && i < GlobalRenderSystem.boundTextureIds.length) {
+            TextureManager textureManager = MinecraftClient.getInstance().getTextureManager();
+            AbstractTexture abstractTexture = textureManager.getTexture(identifier);
+            GlobalRenderSystem.boundTextureIds[i] = abstractTexture.getGlId();
+        }
+        ci.cancel();
+    }
 
-    @Inject(method = "setShaderTexture(II)V", at = @At("HEAD"), remap = false, cancellable = true)
+    @Inject(method = "setShaderTexture(II)V", at = @At("HEAD"), cancellable = true)
     private static void setTextureFromId(int i, int j, CallbackInfo ci) {
         if (i >= 0 && i < GlobalRenderSystem.boundTextureIds.length) {
             GlobalRenderSystem.boundTextureIds[i] = j;
@@ -34,7 +43,7 @@ public class RenderSystemMixin {
         ci.cancel();
     }
 
-    @Inject(method = "getShaderTexture", at = @At("HEAD"), remap = false, cancellable = true)
+    @Inject(method = "getShaderTexture", at = @At("HEAD"), cancellable = true)
     private static void getTextureFromUs(int i, CallbackInfoReturnable<Integer> cir) {
         cir.setReturnValue(i >= 0 && i < GlobalRenderSystem.boundTextureIds.length ? GlobalRenderSystem.boundTextureIds[i] : 0);
     }
