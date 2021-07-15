71d3990614becdd3f4b1c691e7bda71d4330158c
burgerguy <30326913+burgerguy@users.noreply.github.com>
2021-07-15 16:12:07 -0400

    fix jemalloc detection lol

--- src/main/java/me/hydos/blaze4d/Blaze4DPreLaunch.java
+++ src/main/java/me/hydos/blaze4d/Blaze4DPreLaunch.java
@@ -13,7 +13,8 @@ public class Blaze4DPreLaunch implements PreLaunchEntrypoint {
         Configuration.STACK_SIZE.set(LWJGL_STACK_SIZE);
 
         // jemalloc has a memory leak bug on windows from 5.0.0 to 5.2.0
-        if (Platform.get().equals(Platform.WINDOWS) && JEmalloc.JEMALLOC_VERSION_MAJOR == 5 && JEmalloc.JEMALLOC_VERSION_MINOR >= 0 && JEmalloc.JEMALLOC_VERSION_MINOR <= 2 && JEmalloc.JEMALLOC_VERSION_BUGFIX < 1) {
+        if (Platform.get().equals(Platform.WINDOWS) && JEmalloc.JEMALLOC_VERSION_MAJOR == 5 &&
+                (JEmalloc.JEMALLOC_VERSION_MINOR >= 0 && JEmalloc.JEMALLOC_VERSION_MINOR < 2) || (JEmalloc.JEMALLOC_VERSION_MINOR == 2 && JEmalloc.JEMALLOC_VERSION_BUGFIX < 1)) {
             Configuration.MEMORY_ALLOCATOR.set("system");
         }
     }
