1c38bb73e515b6c34cb1939ce20cf3e81c98d24d
burgerguy <30326913+burgerguy@users.noreply.github.com>
2021-07-15 15:54:52 -0400

    disable JEMalloc in situations where it can cause memory leaks

--- src/main/java/me/hydos/blaze4d/Blaze4DPreLaunch.java
+++ src/main/java/me/hydos/blaze4d/Blaze4DPreLaunch.java
@@ -2,6 +2,8 @@ package me.hydos.blaze4d;
 
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 import org.lwjgl.system.Configuration;
+import org.lwjgl.system.Platform;
+import org.lwjgl.system.jemalloc.JEmalloc;
 
 public class Blaze4DPreLaunch implements PreLaunchEntrypoint {
     public static final int LWJGL_STACK_SIZE = 4096; // 4mb instead of default 64kb. TODO: don't rely on a larger stack size
@@ -9,5 +11,10 @@ public class Blaze4DPreLaunch implements PreLaunchEntrypoint {
     @Override
     public void onPreLaunch() {
         Configuration.STACK_SIZE.set(LWJGL_STACK_SIZE);
+
+        // jemalloc has a memory leak bug on windows from 5.0.0 to 5.2.0
+        if (Platform.get().equals(Platform.WINDOWS) && JEmalloc.JEMALLOC_VERSION_MAJOR == 5 && JEmalloc.JEMALLOC_VERSION_MINOR >= 0 && JEmalloc.JEMALLOC_VERSION_MINOR <= 2 && JEmalloc.JEMALLOC_VERSION_BUGFIX < 1) {
+            Configuration.MEMORY_ALLOCATOR.set("system");
+        }
     }
 }
