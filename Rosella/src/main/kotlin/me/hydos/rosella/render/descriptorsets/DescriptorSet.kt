262e51fd93fe1c6f615295349840dec0e91c8f18
OroArmor <eliorona@live.com>
2021-07-14 17:11:31 -0700

    increased fps by about 20% maybe needs more testing

--- Rosella/src/main/kotlin/me/hydos/rosella/render/descriptorsets/DescriptorSet.kt
+++ Rosella/src/main/kotlin/me/hydos/rosella/render/descriptorsets/DescriptorSet.kt
@@ -2,30 +2,34 @@ package me.hydos.rosella.render.descriptorsets
 
 import me.hydos.rosella.device.VulkanDevice
 import org.lwjgl.vulkan.VK10
+import java.util.concurrent.CompletableFuture
 
 class DescriptorSet(var descriptorPool: Long? = null) {
-    var descriptorSets = ArrayList<Long>()
+	var descriptorSets = ArrayList<Long>()
 
-    fun free(device: VulkanDevice) {
-        descriptorPool?.also {
-            for (descriptorSet in descriptorSets) {
-                if (descriptorSet != 0L) {
-                    VK10.vkFreeDescriptorSets(device.rawDevice, it, descriptorSet)
-                }
-            }
+	fun free(device: VulkanDevice) {
+		descriptorPool?.also {
+			val _descriptorSets = ArrayList(descriptorSets)
+			CompletableFuture.runAsync {
+				for (descriptorSet in _descriptorSets) {
+					if (descriptorSet != 0L) {
+						VK10.vkFreeDescriptorSets(device.rawDevice, it, descriptorSet)
+					}
+				}
+			}.complete(null)
+		}
 
-            descriptorSets.clear()
-        }
+		descriptorSets.clear()
 
-        descriptorPool = null
-    }
+		descriptorPool = null
+	}
 
-    fun clear() {
-        descriptorSets.clear()
-        descriptorPool = null
-    }
+	fun clear() {
+		descriptorSets.clear()
+		descriptorPool = null
+	}
 
-    fun add(descriptorSet: Long) {
-        descriptorSets.add(descriptorSet)
-    }
+	fun add(descriptorSet: Long) {
+		descriptorSets.add(descriptorSet)
+	}
 }
