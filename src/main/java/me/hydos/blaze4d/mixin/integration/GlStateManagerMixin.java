f9c53a6bd0fd09e0ffd90ad3033ba80877c1c0d7
burgerguy <30326913+burgerguy@users.noreply.github.com>
2021-07-24 12:38:40 -0400

    don't crash if the device isn't initialized

--- src/main/java/me/hydos/blaze4d/mixin/integration/GlStateManagerMixin.java
+++ src/main/java/me/hydos/blaze4d/mixin/integration/GlStateManagerMixin.java
@@ -7,6 +7,7 @@ import me.hydos.blaze4d.Blaze4D;
 import me.hydos.blaze4d.api.GlobalRenderSystem;
 import me.hydos.blaze4d.api.util.ConversionUtils;
 import me.hydos.rosella.Rosella;
+import me.hydos.rosella.device.VulkanDevice;
 import me.hydos.rosella.scene.object.impl.SimpleObjectManager;
 import me.hydos.rosella.util.Color;
 import org.lwjgl.opengl.GL13;
@@ -223,13 +224,14 @@ public class GlStateManagerMixin {
     @Inject(method = "_getString", at = @At("HEAD"), cancellable = true)
     private static void getString(int glStringId, CallbackInfoReturnable<String> ci) {
         ci.setReturnValue(
-                switch (glStringId) {
-                    case GL.GL_VENDOR -> tryParseVendorId(Blaze4D.rosella.common.device.properties.vendorId);
-                    case GL.GL_EXTENSIONS -> Blaze4D.rosella.common.device.combinedExtensionsString;
-                    case GL.GL_RENDERER -> Blaze4D.rosella.common.device.properties.deviceName;
-                    case GL.GL_VERSION -> "Vulkan API " + Blaze4D.rosella.common.device.properties.apiVersion;
-                    default -> throw new IllegalStateException("Unexpected value: " + glStringId);
-                }
+                Blaze4D.rosella == null ? "Device not initialized" :
+                    switch (glStringId) {
+                        case GL.GL_VENDOR -> tryParseVendorId(Blaze4D.rosella.common.device.properties.vendorId);
+                        case GL.GL_EXTENSIONS -> Blaze4D.rosella.common.device.combinedExtensionsString;
+                        case GL.GL_RENDERER -> Blaze4D.rosella.common.device.properties.deviceName;
+                        case GL.GL_VERSION -> "Vulkan API " + Blaze4D.rosella.common.device.properties.apiVersion;
+                        default -> throw new IllegalStateException("Unexpected value: " + glStringId);
+                    }
         );
     }
 
