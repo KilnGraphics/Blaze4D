50667775e9fc6ad134c6a51b98a96aafee0463ca
hYdos <haydenv06@gmail.com>
2021-09-30 12:29:57 +1000

    e

--- src/main/java/me/hydos/blaze4d/Blaze4DPreLaunch.java
+++ src/main/java/me/hydos/blaze4d/Blaze4DPreLaunch.java
@@ -8,14 +8,14 @@ import oshi.SystemInfo;
 
 public class Blaze4DPreLaunch implements PreLaunchEntrypoint {
     public static final boolean DEBUG_MEMORY_ENABLED = false;
-    public static final int LWJGL_STACK_SIZE = 65535; // 64mb instead of default 64kb. TODO: don't rely on a larger stack size
+    public static final int LWJGL_STACK_SIZE = 65535; // 64mb instead of default 64kb.
 
     @Override
     public void onPreLaunch() {
         Configuration.DEBUG_MEMORY_ALLOCATOR.set(DEBUG_MEMORY_ENABLED);
         Configuration.STACK_SIZE.set(LWJGL_STACK_SIZE);
 
-        // jemalloc has a memory leak bug on windows from 5.0.0 to 5.2.0
+        // jemalloc has a memory leak bug on Windows from 5.0.0 to 5.2.0
         if (Platform.get().equals(Platform.WINDOWS) && JEmalloc.JEMALLOC_VERSION_MAJOR == 5 &&
                 (JEmalloc.JEMALLOC_VERSION_MINOR >= 0 && JEmalloc.JEMALLOC_VERSION_MINOR < 2) || (JEmalloc.JEMALLOC_VERSION_MINOR == 2 && JEmalloc.JEMALLOC_VERSION_BUGFIX < 1)) {
             Configuration.MEMORY_ALLOCATOR.set("system");
