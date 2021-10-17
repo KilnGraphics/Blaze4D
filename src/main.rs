364b0629bfad479e2169d4bd05f5f9f7f2aa49fb
hYdos <haydenv06@gmail.com>
2021-10-18 08:15:58 +1000

    Cope.

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+mod rosella;
+
+use std::ffi::CString;
+use ash::Entry;
+use ash::extensions::ext::DebugUtils;
+use winit::dpi::LogicalSize;
+use winit::event_loop::EventLoop;
+use winit::window::WindowBuilder;
+use rosella::window::RosellaWindow;
+
+unsafe fn main() {
+    RosellaWindow::new("Rosella Rust TM", f64::from(800), f64::from(600));
+
+    let vk = Entry::new();
+    let application_name = CString::new("Rosella In Rust Test. Keep Malding");
+
+    let surface_extensions = ash_window::enumerate_required_extensions(&window).unwrap();
+    let mut extension_names_raw = surface_extensions
+        .iter()
+        .map(|ext| ext.as_ptr())
+        .collect::<Vec<_>>();
+    extension_names_raw.push(DebugUtils::name().as_ptr());
+
+    let debug_utils_loader = DebugUtils::new(&entry, &instance);
+    let debug_call_back = debug_utils_loader
+        .create_debug_utils_messenger(&debug_info, None)
+        .unwrap();
+}
---
+++ src/rosella/window.rs
@@ -0,0 +1,27 @@
+use winit::dpi::LogicalSize;
+use winit::event_loop::EventLoop;
+use winit::window::WindowBuilder;
+
+pub struct RosellaWindow {
+    event_loop: EventLoop<()>,
+    window: winit::window::Window,
+}
+
+impl RosellaWindow {
+    pub fn new(title: &str, width: f64, height: f64) -> RosellaWindow {
+        let event_loop = EventLoop::new();
+        let window = WindowBuilder::new()
+            .with_title(title)
+            .with_inner_size(LogicalSize::new(
+                width,
+                height,
+            ))
+            .build(&event_loop)
+            .unwrap();
+
+        RosellaWindow {
+            event_loop,
+            window,
+        }
+    }
+}
