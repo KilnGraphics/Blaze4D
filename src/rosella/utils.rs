3265581b33b1db7694b83c6361b78ea583ccc8ca
hYdos <haydenv06@gmail.com>
2021-10-22 19:29:37 +1000

    Start to do generate_enabled_extension_names

--- src/rosella/utils.rs
+++ src/rosella/utils.rs
@@ -1,7 +1,12 @@
 use std::ffi::CStr;
+use std::os::raw::c_char;
 
 ///
 pub(crate) fn string_from_array(slice: &[i8]) -> String {
     let u8_array: &[u8] = unsafe { std::mem::transmute(slice) };
     unsafe { String::from_utf8_unchecked(Vec::from(CStr::from_ptr(u8_array.as_ptr() as *const _).to_bytes())) }
+}
+
+pub(crate) fn string_to_array(string: String) -> *const c_char {
+    string.as_ptr() as *const c_char
 }
