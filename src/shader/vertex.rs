57c6334d2203215204038809829c98d1795c5e42
hYdos <haydenv06@gmail.com>
2021-10-27 19:46:12 +1000

    Genius

--- src/shader/vertex.rs
+++ src/shader/vertex.rs
@@ -2,7 +2,7 @@
 pub mod data_type {
     use std::mem::size_of;
 
-    const FLOAT: usize = size_of::<F32>();
+    const FLOAT: usize = size_of::<f32>();
 }
 
 /// The format in which vertex data is stored. For example if you where storing position and color per Vertex, You may store it as 2 vec3's
