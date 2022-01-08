65ca6e30ef5130db4fe9e2419f877d00663e9b09
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2022-01-08 18:35:53 +0100

    Added worker thread to device context.

--- src/device.rs
+++ src/device.rs
@@ -1,11 +1,57 @@
 use std::sync::Arc;
+use std::thread::JoinHandle;
 
 use ash::vk;
+use concurrent_queue::ConcurrentQueue;
 
 use crate::init::EnabledFeatures;
 use crate::instance::InstanceContext;
 use crate::util::extensions::{AsRefOption, ExtensionFunctionSet, VkExtensionInfo, VkExtensionFunctions};
 use crate::UUID;
+
+struct WorkerData {
+    queue: ConcurrentQueue<()>,
+}
+
+impl WorkerData {
+    fn new() -> Self {
+        Self {
+            queue: ConcurrentQueue::unbounded(),
+        }
+    }
+
+    /// After this function is called no new requests may be pushed into the worker
+    pub fn close(&self) {
+        self.queue.close();
+    }
+}
+
+struct Worker {
+    thread: Option<JoinHandle<()>>,
+    data: Arc<WorkerData>,
+}
+
+impl Worker {
+    pub fn spawn() -> Self {
+        let data = Arc::new(WorkerData::new());
+        let cloned_data = data.clone();
+        Self {
+            thread: Some(std::thread::spawn(move|| Self::run(cloned_data))),
+            data,
+        }
+    }
+
+    fn run(data: Arc<WorkerData>) {
+
+    }
+}
+
+impl Drop for Worker {
+    fn drop(&mut self) {
+        self.data.close();
+        self.thread.take().unwrap().join().unwrap();
+    }
+}
 
 pub struct DeviceContextImpl {
     instance: InstanceContext,
@@ -13,6 +59,7 @@ pub struct DeviceContextImpl {
     physical_device: vk::PhysicalDevice,
     extensions: ExtensionFunctionSet,
     features: EnabledFeatures,
+    worker: Worker,
 }
 
 impl Drop for DeviceContextImpl {
@@ -34,6 +81,7 @@ impl DeviceContext {
             physical_device,
             extensions,
             features,
+            worker: Worker::spawn(),
         }))
     }
 
