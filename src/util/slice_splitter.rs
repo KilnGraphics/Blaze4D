cd01c3ff2565e2ce4816851158e3c17717c59800
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2022-01-09 13:31:48 +0100

    Splitter documentation

--- src/util/slice_splitter.rs
+++ src/util/slice_splitter.rs
@@ -1,3 +1,5 @@
+/// Enables mutable access to one element of a slice while still providing immutable access
+/// to other elements.
 pub struct Splitter<'a, T> {
     index: usize,
     head: &'a [T],
@@ -5,6 +7,7 @@ pub struct Splitter<'a, T> {
 }
 
 impl<'a, T> Splitter<'a, T> {
+    /// Creates a new Splitter providing mutable access to the element at the specified index.
     pub fn new<'b: 'a>(slice: &'b mut [T], index: usize) -> (Self, &'a mut T) {
         let (head, tail) = slice.split_at_mut(index);
         let (elem, tail) = tail.split_first_mut().unwrap();
@@ -16,6 +19,7 @@ impl<'a, T> Splitter<'a, T> {
         }, elem)
     }
 
+    /// Returns a reference to any element that is not mutably accessed.
     pub fn get(&self, index: usize) -> Option<&T> {
         if index < self.index {
             self.head.get(index)
