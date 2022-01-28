e3c847e3675f4959d4cc962fe5af9622714e7658
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2022-01-28 17:35:36 +0100

    More documentation

--- src/objects/manager/object_set.rs
+++ src/objects/manager/object_set.rs
@@ -12,36 +12,73 @@ use crate::objects::image::{ImageInfo, ImageViewInfo};
 
 /// A trait that must be implemented by any object set implementation.
 pub trait ObjectSetProvider {
+    /// Returns the id of this object set.
     fn get_id(&self) -> ObjectSetId;
 
+    /// Returns the handle of a buffer that is part of this object set.
+    ///
+    /// #Panics
+    /// If the buffer id does not belong to this object set or does not map to a buffer object.
     fn get_buffer_handle(&self, _: id::BufferId) -> vk::Buffer {
         panic!("ObjectSet does not support buffers");
     }
 
+    /// Returns the [`BufferInfo`] struct for a buffer that is part of this object set.
+    ///
+    /// #Panics
+    /// If the buffer id does not belong to this object set or does not map to a buffer object.
     fn get_buffer_info(&self, _: id::BufferId) -> &Arc<BufferInfo> {
         panic!("ObjectSet does not support buffers");
     }
 
+    /// Returns the handle of a buffer view that is part of this object set.
+    ///
+    /// #Panics
+    /// If the buffer view id does not belong to this object set or does not map to a buffer view
+    /// object.
     fn get_buffer_view_handle(&self, _: id::BufferViewId) -> vk::BufferView {
         panic!("ObjectSet does not support buffer views");
     }
 
+    /// Returns the [`BufferViewInfo`] struct for a buffer view that is part of this object set.
+    ///
+    /// #Panics
+    /// If the buffer view id does not belong to this object set or does not map to a buffer view
+    /// object.
     fn get_buffer_view_info(&self, _: id::BufferViewId) -> &BufferViewInfo {
         panic!("ObjectSet does not support buffer views");
     }
 
+    /// Returns the handle of a image that is part of this object set.
+    ///
+    /// #Panics
+    /// If the image id does not belong to this object set or does not map to a image object.
     fn get_image_handle(&self, _: id::ImageId) -> vk::Image {
         panic!("ObjectSet does not support images");
     }
 
+    /// Returns the [`ImageInfo`] struct for a image that is part of this object set.
+    ///
+    /// #Panics
+    /// If the image id does not belong to this object set or does not map to a image object.
     fn get_image_info(&self, _: id::ImageId) -> &Arc<ImageInfo> {
         panic!("ObjectSet does not support images");
     }
 
+    /// Returns the handle of a image view that is part of this object set.
+    ///
+    /// #Panics
+    /// If the image view id does not belong to this object set or does not map to a image view
+    /// object.
     fn get_image_view_handle(&self, _: id::ImageViewId) -> vk::ImageView {
         panic!("ObjectSet does not support image views");
     }
 
+    /// Returns the [`ImageViewInfo`] struct for a image view that is part of this object set.
+    ///
+    /// #Panics
+    /// If the image view id does not belong to this object set or does not map to a image view
+    /// object.
     fn get_image_view_info(&self, _: id::ImageViewId) -> &ImageViewInfo {
         panic!("ObjectSet does not support image views");
     }
