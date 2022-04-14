05d7a1ea37e350820081619034843345f4d00ee0
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2022-04-15 01:15:47 +0200

    Added log messenger

--- core/src/util/debug_messenger.rs
+++ core/src/util/debug_messenger.rs
@@ -1,5 +1,6 @@
 use std::ffi::CStr;
 use ash::vk;
+use ash::vk::{DebugUtilsMessageSeverityFlagsEXT, DebugUtilsMessageTypeFlagsEXT, DebugUtilsMessengerCallbackDataEXT};
 
 pub trait DebugMessengerCallback {
     fn on_message(
@@ -11,5 +12,26 @@ pub trait DebugMessengerCallback {
     );
 }
 
-pub struct StdOutDebugMessenger {
+pub struct RustLogDebugMessenger {
+}
+
+impl RustLogDebugMessenger {
+    pub fn new() -> Self {
+        Self {
+        }
+    }
+}
+
+impl DebugMessengerCallback for RustLogDebugMessenger {
+    fn on_message(&self, message_severity: DebugUtilsMessageSeverityFlagsEXT, message_types: DebugUtilsMessageTypeFlagsEXT, message: &CStr, data: &DebugUtilsMessengerCallbackDataEXT) {
+        if message_severity.contains(DebugUtilsMessageSeverityFlagsEXT::ERROR) {
+            log::error!("{:?}", message);
+        } else if message_severity.contains(DebugUtilsMessageSeverityFlagsEXT::WARNING) {
+            log::warn!("{:?}", message);
+        } else if message_severity.contains(DebugUtilsMessageSeverityFlagsEXT::INFO) {
+            log::info!("{:?}", message);
+        } else {
+            log::info!("Unknown severity: {:?}", message);
+        }
+    }
 }
--- core/tests/headless_init.rs
+++ core/tests/headless_init.rs
@@ -4,19 +4,23 @@ use ash::vk;
 
 use b4d_core::init::instance::*;
 use b4d_core::rosella::VulkanVersion;
+use b4d_core::util::debug_messenger::RustLogDebugMessenger;
 
 mod test_common;
 
 #[test]
 fn init_no_feature() {
+    env_logger::init();
+
     let profile = vp::LunargDesktopPortability2021::profile_properties();
 
-    let config = InstanceCreateConfig::new(
+    let mut config = InstanceCreateConfig::new(
         profile,
         VulkanVersion::VK_1_1,
         CString::from(CStr::from_bytes_with_nul(b"B4D_Test\0").unwrap()),
         vk::make_api_version(0, 0, 1, 0)
     );
+    config.add_debug_messenger(Box::new(RustLogDebugMessenger::new()));
 
     let instance = create_instance(config).unwrap();
 }
