dcee72ef17fe39644c2539286aaf5be0784f55b4
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2022-05-02 00:10:01 +0200

    Added shader build script

--- resources/build.gradle.kts
+++ resources/build.gradle.kts
@@ -1,46 +1,111 @@
 enum class ShaderType(val cliString: String) {
-    VERTEX("vertex"),
-    FRAGMENT("fragment"),
-    TESSELATION_CONTROL("tesscontrol"),
-    TESSELATION_EVALUATION("tesseval"),
-    GEOMETRY("geometry"),
-    COMPUTE("compute"),
+    AUTO(""),
+    VERTEX("-fshader-stage=vertex"),
+    FRAGMENT("-fshader-stage=fragment"),
+    TESSELATION_CONTROL("-fshader-stage=tesscontrol"),
+    TESSELATION_EVALUATION("-fshader-stage=tesseval"),
+    GEOMETRY("-fshader-stage=geometry"),
+    COMPUTE("-fshader-stage=compute"),
 }
 
-class ShaderModule(source: File, type: ShaderType?) {
-    var source: File = source;
-    var type: ShaderType? = type;
-}
-
-abstract class CompileShaders : DefaultTask() {
-    private var sources: ArrayList<ShaderModule> = ArrayList();
-
-    fun shader(src: Any) {
-        this.shader(src, null)
+public abstract class ShaderModule {
+    private val name: String;
+    public fun getName(): String {
+        return this.name;
     }
 
-    fun shader(src: Any, type: ShaderType?) {
-        var file = project.file(src, PathValidation.FILE);
+    @get:Input
+    public abstract val source: Property<RelativePath>;
 
-        this.sources.add(ShaderModule(file, type));
+    @get:Input
+    public abstract val output: Property<RelativePath>;
+
+    @get:Input
+    @Optional
+    public val type: Property<ShaderType>;
+
+    @Inject
+    constructor(name: String, objectFactory: ObjectFactory) {
+        this.name = name;
+
+        this.type = objectFactory.property(ShaderType::class);
+        this.type.convention(ShaderType.AUTO);
+    }
+
+    public fun source(src: String) {
+        this.source(RelativePath(true, src));
+    }
+
+    public fun source(src: RelativePath) {
+        this.source.set(src);
+    }
+
+    public fun output(dst: String) {
+        this.output(RelativePath(true, dst));
+    }
+
+    public fun output(dst: RelativePath) {
+        this.output.set(dst);
+    }
+}
+
+public abstract class CompileShaders : DefaultTask() {
+    @get:Input
+    public abstract val baseDir: Property<RelativePath>;
+
+    @get:Input
+    public abstract val modules: NamedDomainObjectContainer<ShaderModule>;
+
+    @get:Input
+    public abstract val includeDirs: SetProperty<File>;
+
+    @Inject
+    protected abstract fun getObjectFactory(): ObjectFactory;
+
+    fun include(dir: Any) {
+        this.includeDirs.add(project.file(dir, PathValidation.DIRECTORY));
+    }
+
+    fun addModule(name: String, action: Action<in ShaderModule>) {
+        this.modules.create(name, action);
     }
 
     @TaskAction
     fun run() {
-        this.sources.forEach {
-            project.exec({
-                executable("glslc")
+        var base = this.baseDir.get();
+        var basePath = base.getFile(project.getProjectDir());
 
-                it.type?.let { type -> 
-                    args("-fshader-stage=${type.cliString}")
+        this.modules.forEach {
+            var source = base.plus(it.source.get()).getFile(project.getProjectDir());
+            var output = base.plus(it.output.get()).getFile(project.getBuildDir());
+            var type = it.type.get();
+            output.getParentFile().mkdirs();
+
+            project.exec({
+                executable("glslc");
+
+                if (type != ShaderType.AUTO) {
+                    args(type.cliString);
                 }
 
-                args(it.source)
+                args("-I${basePath}");
+                
+                args("-o${output}");
+                args("${source}");
             });
         }
     }
 }
 
-tasks.register<CompileShaders>("compileShaders") {
-    shader("debug/apply.vert", ShaderType.VERTEX)
+tasks.register<CompileShaders>("compileDebugShaders") {
+    baseDir.set(RelativePath(false, "debug"));
+
+    addModule("ApplyVert", {
+        source("apply.vert")
+        output("apply_vert.spv")
+    });
+    addModule("ApplyFrag", {
+        source("apply.frag")
+        output("apply_frag.spv")
+    });
 }
