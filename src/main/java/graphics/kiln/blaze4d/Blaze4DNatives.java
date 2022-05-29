c251da5527f430c95dbd68d318733b4cc8c8d456
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2022-05-29 18:49:38 +0200

    Finish native loading and api

--- src/main/java/graphics/kiln/blaze4d/Blaze4DNatives.java
+++ src/main/java/graphics/kiln/blaze4d/Blaze4DNatives.java
@@ -5,70 +5,183 @@ import jdk.incubator.foreign.*;
 import java.lang.invoke.MethodHandle;
 import java.util.Optional;
 
+import static jdk.incubator.foreign.ValueLayout.*;
+
 public class Blaze4DNatives {
 
     private static SymbolLookup lookup;
     private static CLinker linker;
 
-    public static MethodHandle b4dEmulatorVertexFormatSetBuilderNew;
-    public static MethodHandle b4dEmulatorVertexFormatSetBuilderDestroy;
-    public static MethodHandle b4dEmulatorVertexFormatSetBuilderAddFormat;
+    public static MethodHandle b4dInitHandle;
+    public static MethodHandle b4dDestroyHandle;
+    public static MethodHandle b4dSetVertexFormatsHandle;
+    public static MethodHandle b4dCreateStaticMeshHandle;
+    public static MethodHandle b4dDestroyStaticMeshHandle;
+    public static MethodHandle b4dStartFrameHandle;
 
-    public static MethodHandle b4dInit;
-    public static MethodHandle b4dDestroy;
-    public static MethodHandle b4dSetVertexFormats;
-    public static MethodHandle b4dCreateStaticMesh;
-    public static MethodHandle b4dDestroyStaticMesh;
-    public static MethodHandle b4dStartFrame;
+    public static MethodHandle b4dPassSetModelViewMatrixHandle;
+    public static MethodHandle b4dPassSetProjectionMatrixHandle;
+    public static MethodHandle b4dPassDrawStaticHandle;
+    public static MethodHandle b4dPassDrawImmediateHandle;
+    public static MethodHandle b4dEndFrameHandle;
 
-    public static MethodHandle b4dPassSetModelViewMatrix;
-    public static MethodHandle b4dPassSetProjectionMatrix;
-    public static MethodHandle b4dPassDrawStatic;
-    public static MethodHandle b4dPassDrawImmediate;
-    public static MethodHandle b4dEndFrame;
-
-    public static SequenceLayout meshDataLayout;
-    public static SequenceLayout b4dVertexFormatLayout;
+    public static GroupLayout meshDataLayout;
+    public static GroupLayout vertexFormatLayout;
 
     static void load() {
+        System.loadLibrary("b4d_core");
+
         lookup = SymbolLookup.loaderLookup();
         linker = CLinker.systemCLinker();
 
-        b4dEmulatorVertexFormatSetBuilderNew = lookupFunction("b4d_emulator_vertex_format_set_builder_new",
-                FunctionDescriptor.of(ValueLayout.ADDRESS)
+        meshDataLayout = MemoryLayout.structLayout(
+                ADDRESS,
+                JAVA_LONG,
+                ADDRESS,
+                JAVA_LONG,
+                JAVA_INT,
+                JAVA_INT
         );
 
-        b4dEmulatorVertexFormatSetBuilderDestroy = lookupFunction("b4d_emulator_vertex_format_set_builder_destroy",
-                FunctionDescriptor.ofVoid(ValueLayout.ADDRESS)
+        vertexFormatLayout = MemoryLayout.structLayout(
+                JAVA_INT,
+                JAVA_INT,
+                JAVA_INT,
+                JAVA_INT
         );
 
-        b4dEmulatorVertexFormatSetBuilderAddFormat = lookupFunction("b4d_emulator_vertex_format_builder_add_format",
-                FunctionDescriptor.ofVoid(ValueLayout.ADDRESS, ValueLayout.JAVA_INT)
+        b4dInitHandle = lookupFunction("b4d_init",
+                FunctionDescriptor.of(ADDRESS, ADDRESS, JAVA_INT)
         );
 
-        b4dInit = lookupFunction("b4d_init",
-                FunctionDescriptor.of(ValueLayout.ADDRESS, ValueLayout.ADDRESS, ValueLayout.ADDRESS, ValueLayout.JAVA_INT)
+        b4dDestroyHandle = lookupFunction("b4d_destroy",
+                FunctionDescriptor.ofVoid(ADDRESS)
         );
 
-        b4dDestroy = lookupFunction("b4d_destroy",
-                FunctionDescriptor.ofVoid(ValueLayout.ADDRESS)
+        b4dSetVertexFormatsHandle = lookupFunction("b4d_set_vertex_formats",
+                FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, JAVA_INT)
         );
 
-        b4dSetVertexFormats = lookupFunction("b4d_set_vertex_formats",
-                FunctionDescriptor.ofVoid(ValueLayout.ADDRESS, ValueLayout.ADDRESS, ValueLayout.JAVA_INT)
+        b4dCreateStaticMeshHandle = lookupFunction("b4d_create_static_mesh",
+                FunctionDescriptor.of(JAVA_LONG, ADDRESS, ADDRESS)
         );
 
-        b4dCreateStaticMesh = lookupFunction("b4d_create_static_mesh",
-                FunctionDescriptor.of(ValueLayout.JAVA_LONG, ValueLayout.ADDRESS, ValueLayout.ADDRESS)
+        b4dDestroyStaticMeshHandle = lookupFunction("b4d_destroy_static_mesh",
+                FunctionDescriptor.ofVoid(ADDRESS, JAVA_LONG)
         );
 
-        b4dDestroyStaticMesh = lookupFunction("b4d_destroy_static_mesh",
-                FunctionDescriptor.ofVoid(ValueLayout.ADDRESS, ValueLayout.JAVA_LONG)
+        b4dStartFrameHandle = lookupFunction("b4d_start_frame",
+                FunctionDescriptor.of(ADDRESS, ADDRESS, JAVA_INT, JAVA_INT)
         );
 
-        b4dStartFrame = lookupFunction("b4d_start_frame",
-                FunctionDescriptor.of(ValueLayout.ADDRESS, ValueLayout.ADDRESS, ValueLayout.JAVA_INT, ValueLayout.JAVA_INT)
+        b4dPassSetModelViewMatrixHandle = lookupFunction("b4d_pass_set_model_view_matrix",
+                FunctionDescriptor.ofVoid(ADDRESS, ADDRESS)
         );
+
+        b4dPassSetProjectionMatrixHandle = lookupFunction("b4d_pass_set_projection_matrix",
+                FunctionDescriptor.ofVoid(ADDRESS, ADDRESS)
+        );
+
+        b4dPassDrawStaticHandle = lookupFunction("b4d_pass_draw_static",
+                FunctionDescriptor.ofVoid(ADDRESS, JAVA_LONG, JAVA_INT)
+        );
+
+        b4dPassDrawImmediateHandle = lookupFunction("b4d_pass_draw_immediate",
+                FunctionDescriptor.ofVoid(ADDRESS, ADDRESS, JAVA_INT)
+        );
+
+        b4dEndFrameHandle = lookupFunction("b4d_end_frame",
+                FunctionDescriptor.ofVoid(ADDRESS)
+        );
+    }
+
+    public static MemoryAddress b4dInit(MemoryAddress surface, boolean enableValidation) {
+        int validation = enableValidation ? 1 : 0;
+        try {
+            return (MemoryAddress) b4dInitHandle.invokeExact(surface, validation);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void b4dDestroy(MemoryAddress b4d) {
+        try {
+            b4dDestroyHandle.invokeExact(b4d);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void b4dSetVertexFormats(MemoryAddress b4d, MemoryAddress vertexFormats, int formatCount) {
+        try {
+            b4dSetVertexFormatsHandle.invokeExact(b4d, vertexFormats, formatCount);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static long b4dCreateStaticMesh(MemoryAddress b4d, MemoryAddress meshData) {
+        try {
+            return (long) b4dCreateStaticMeshHandle.invokeExact(b4d, meshData);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void b4dDestroyStaticMesh(MemoryAddress b4d, long meshId) {
+        try {
+            b4dDestroyStaticMeshHandle.invokeExact(b4d, meshId);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static MemoryAddress b4dStartFrame(MemoryAddress b4d, int windowWidth, int windowHeight) {
+        try {
+            return (MemoryAddress) b4dStartFrameHandle.invokeExact(b4d, windowWidth, windowHeight);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void b4dPassSetModelViewMatrix(MemoryAddress pass, MemoryAddress matrix) {
+        try {
+            b4dPassSetModelViewMatrixHandle.invokeExact(pass, matrix);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void b4dPassSetProjectionMatrix(MemoryAddress pass, MemoryAddress matrix) {
+        try {
+            b4dPassSetProjectionMatrixHandle.invokeExact(pass, matrix);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void b4dPassDrawStatic(MemoryAddress pass, long meshId, int typeId) {
+        try {
+            b4dPassDrawStaticHandle.invokeExact(pass, meshId, typeId);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void b4dPassDrawImmediate(MemoryAddress pass, MemoryAddress meshData, int typeId) {
+        try {
+            b4dPassDrawImmediateHandle.invokeExact(pass, meshData, typeId);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void b4dEndFrame(MemoryAddress pass) {
+        try {
+            b4dEndFrameHandle.invokeExact(pass);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static MethodHandle lookupFunction(String name, FunctionDescriptor descriptor) {
