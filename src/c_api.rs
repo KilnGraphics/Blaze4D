ef460d3d8ffd55ab5774eab0a2d2154691bf80a0
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2022-05-29 13:02:40 +0200

    Completed initial c api

--- src/c_api.rs
+++ src/c_api.rs
@@ -4,7 +4,7 @@ use ash::vk;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::platform::run_return::EventLoopExtRunReturn;
-use crate::b4d::Blaze4D;
+use crate::b4d::{B4DVertexFormat, Blaze4D};
 use crate::glfw_surface::GLFWSurfaceProvider;
 use crate::prelude::{Mat4f32, UUID, Vec2u32};
 
@@ -29,6 +29,26 @@ impl CMeshData {
             index_count: self.index_count,
             index_type: vk::IndexType::UINT32,
             vertex_format_id: VertexFormatId::from_raw(self.vertex_format_id),
+        }
+    }
+}
+
+#[repr(C)]
+struct CB4DVertexFormat {
+    topology: i32,
+    stride: u32,
+    position_offset: u32,
+    position_format: i32,
+}
+
+impl CB4DVertexFormat {
+    fn to_b4d_vertex_format(&self) -> B4DVertexFormat {
+        B4DVertexFormat {
+            topology: vk::PrimitiveTopology::from_raw(self.topology),
+            stride: self.stride,
+            position: (self.position_offset, vk::Format::from_raw(self.position_format)),
+            color: None,
+            uv: None
         }
     }
 }
@@ -112,6 +132,27 @@ unsafe extern "C" fn b4d_destroy(b4d: *mut B4DDebugWrapper) {
         Box::from_raw(b4d);
     }).unwrap_or_else(|_| {
         log::error!("panic in b4d_destroy");
+        exit(1);
+    })
+}
+
+unsafe extern "C" fn b4d_set_vertex_formats(b4d: *const B4DDebugWrapper, formats_ptr: *const CB4DVertexFormat, formats_len: u32) {
+    catch_unwind(|| {
+        let b4d = b4d.as_ref().unwrap_or_else(|| {
+            log::error!("Passed null b4d to b4d_create_static_mesh");
+            exit(1);
+        });
+        if formats_ptr.is_null() {
+            log::error!("Passed null formats_ptr to b4d_set_vertex_formats");
+            exit(1);
+        }
+        let formats = std::slice::from_raw_parts(formats_ptr, formats_len as usize);
+
+        let formats = formats.iter().map(|format| format.to_b4d_vertex_format()).collect();
+
+        b4d.b4d.set_emulator_vertex_formats(formats);
+    }).unwrap_or_else(|_| {
+        log::error!("panic in b4d_set_vertex_formats");
         exit(1);
     })
 }
